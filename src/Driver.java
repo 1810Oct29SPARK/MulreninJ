f2638418c37c1354acd8850f8274a7a169313ffb
Jacob Mulrenin <jmulrenin@gmail.com>
2018-11-15 16:58:24 -0500

    updated Driver class

--- src/Driver.java
+++ src/Driver.java
@@ -7,6 +7,11 @@ public class Driver{
 		Car carOne = new Car();
 		//toString is a method inherited from Object
 		System.out.println("carOne: " + carOne.toString());
-
+		
+		//make a car object with insance variables set in the constructor
+		//declare the variable carTwo and assign a new Car to it
+		Car carTwo = new Car(1972, "DeLorean", "DMC");
+		//check out the toString result of this
+		System.out.println("carTwo: " + carTwo.toString());
 	}
 }
